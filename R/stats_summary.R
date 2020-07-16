aee6271231414cc47457e00d52595041a330fb50
Kenta Yoshida <6.kurabupasu@gmail.com>
2020-07-16 12:13:09 -0700

    Implement calc_stat_lognormal

--- R/stats_summary.R
+++ R/stats_summary.R
@@ -1,30 +1,74 @@
 #' Calculate summary metrics for a numeric vector
 #'
-#' This function assumes the numbers follow normal distribution
-#'
+#' @describeIn calc_stat_normal Calculate summary for normal distribution
 #' @export
 #' @param x A numeric vector
 #' @return A dataframe containing summary metrics
 #' @examples
-#' add(1, 1)
-#' add(10, 1)
+#' calc_stat_normal(1:10)
 calc_stat_normal <- function(x, ci = 0.95, na.rm = FALSE) {
 
   x_name <- rlang::quo_name(rlang::enquo(x))
   x_name_stat <- paste0(x_name, "_stat")
 
-  ## Calc CI: https://github.com/Novartis/xgxr/blob/master/R/xgx_conf_int.R
+  if(na.rm) {
+    x <- stats::na.omit(x)
+  } else {
+    if (any(is.na(x))) stop("Use `na.rm = TRUE` to ignore NA values")
+  }
+
+  # Calc CI: https://github.com/Novartis/xgxr/blob/master/R/xgx_conf_int.R
   percentile_value <- ci + (1 - ci) / 2
 
-  mu  <- mean(x, na.rm = na.rm)
-  sd  <- stats::sd(x, na.rm = na.rm)
+  mu  <- mean(x)
+  sd  <- stats::sd(x)
   cv  <- sd / mu
   qtt <- stats::qt(percentile_value, length(x))
-  s_v <- sqrt(stats::var(x, na.rm = na.rm) / length(x))
+  s_v <- sqrt(stats::var(x) / length(x))
 
   tibble::tibble(!!x_name := c(mu, sd, cv,
                                mu - qtt * s_v,
                                mu + qtt * s_v),
                  !!x_name_stat := c("mean", "sd", "cv",
                                     "ci_lower", "ci_upper"))
+}
+
+
+#' @describeIn calc_stat_normal Calculate summary for lognormal distribution
+#' @export
+#' @examples
+#' calc_stat_lognormal(1:10)
+calc_stat_lognormal <- function(x, ci = 0.95, na.rm = FALSE) {
+
+  x_name <- rlang::quo_name(rlang::enquo(x))
+  x_name_stat <- paste0(x_name, "_stat")
+
+  if (any(x <= 0, na.rm = TRUE)) stop("Input cannot contain non-positive numbers")
+
+  if(na.rm) {
+    x <- stats::na.omit(x)
+  } else {
+    if (any(is.na(x))) stop("Use `na.rm = TRUE` to ignore NA values")
+  }
+
+  # Calc CI: https://github.com/Novartis/xgxr/blob/master/R/xgx_conf_int.R
+  # Calc geo CV https://github.com/billdenney/pknca/blob/master/R/002-pk.business.rules.R
+
+  percentile_value <- ci + (1 - ci) / 2
+
+  xlog <- log(x)
+
+  mu  <- sum(xlog) / length(x)
+  sd  <- stats::sd(xlog)
+  cv  <- sqrt(exp(stats::sd(xlog) ^ 2) - 1)
+  qtt <- stats::qt(percentile_value, length(xlog))
+  s_v <- sqrt(stats::var(xlog) / length(xlog))
+
+  tibble::tibble(!!x_name := c(exp(mu),
+                               exp(sd),
+                               cv,
+                               exp(mu - qtt * s_v),
+                               exp(mu + qtt * s_v)),
+                 !!x_name_stat := c("geomean", "geosd", "geocv",
+                                    "geoci_lower", "geoci_upper"))
 }
--- tests/testthat/test-stats_summary.R
+++ tests/testthat/test-stats_summary.R
@@ -2,18 +2,42 @@ set.seed(1234)
 xx <- stats::rnorm(30, 5, 1)
 
 xx.calc_stat_normal <- calc_stat_normal(xx)
+xx.calc_stat_lognormal <- calc_stat_lognormal(xx)
 
 test_that("CI matches with Rmisc", {
   ci.calc_stat_normal <- xx.calc_stat_normal$xx[c(5, 1, 4)]
 
   # https://rcompanion.org/handbook/C_03.html
-  ci.rmisc <- Rmisc::CI(x, ci = 0.95)
+  ci.rmisc <- Rmisc::CI(xx, ci = 0.95)
   names(ci.rmisc) <- NULL
 
   expect_equal(ci.calc_stat_normal,
-               ci.rmisc, tolerance = 0.001, scale = x)
+               ci.rmisc,
+               tolerance = 0.001, scale = ci.rmisc)
 })
 
-test_that("error with calc_stat_lognormal if non-positive exist", {
+test_that("geoCI matches with DescTools", {
+  ci.calc_stat_lognormal <- xx.calc_stat_lognormal$xx[c(1, 4, 5)]
 
+  # https://rcompanion.org/handbook/C_03.html
+  ci.desctools <- DescTools::Gmean(xx, conf.level = 0.95)
+  names(ci.desctools) <- NULL
+
+  expect_equal(ci.calc_stat_lognormal,
+               ci.desctools,
+               tolerance = 0.001, scale = ci.desctools)
+})
+
+test_that("CV matches with PKNCA", {
+  cv.calc_stat_normal <- xx.calc_stat_lognormal$xx[3]
+
+  expect_equal(cv.calc_stat_normal,
+               PKNCA::geocv(xx) / 100,
+               tolerance = 0.001, scale = cv.calc_stat_normal)
+})
+
+test_that("error if NA or non-positive (for lognormal) exist", {
+  expect_error(calc_stat_normal(c(1, NA)), "Use `na.rm = TRUE`")
+  expect_error(calc_stat_lognormal(c(1, NA)), "Use `na.rm = TRUE`")
+  expect_error(calc_stat_lognormal(c(1, 0)), "Input cannot contain non-positive numbers")
 })
